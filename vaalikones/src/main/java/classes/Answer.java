e4ffb5963f660b7f285d775158757c8dafef30c8
bocek <bocek83@seznam.cz>
2019-09-09 11:10:24 +0300

    Save methods added to data classes

--- vaalikones/src/main/java/classes/Answer.java
+++ vaalikones/src/main/java/classes/Answer.java
@@ -1,10 +1,10 @@
 package classes;
 
 import java.io.Serializable;
+import java.sql.*;
 import java.util.logging.Logger;
 
 /**
- *
  * @author Bocceli
  */
 
@@ -20,6 +20,22 @@ public class Answer implements Serializable {
 
     public Answer(Integer answerId, String comment) {
         this.answerId = answerId;
+        this.comment = comment;
+    }
+
+    public Integer getAnswerId() {
+        return answerId;
+    }
+
+    public void setAnswerId(Integer answerId) {
+        this.answerId = answerId;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
         this.comment = comment;
     }
 
@@ -51,4 +67,38 @@ public class Answer implements Serializable {
 
     private static final Logger LOG = Logger.getLogger(Answer.class.getName());
 
+    /**
+     * Save method of object
+     * @param conn Connection to database
+     * @return Saved object or null
+     */
+    public Answer save(Connection conn) {
+        try {
+            if (conn != null) {
+                PreparedStatement pstmt = conn.prepareStatement(
+                        "insert into ANSWERS(COMMENT) values(?)",
+                        Statement.RETURN_GENERATED_KEYS
+                );
+                pstmt.setString(1, this.comment);
+
+                pstmt.executeUpdate();
+
+                //getting last inserted id
+                ResultSet rs = pstmt.getGeneratedKeys();
+                if (rs.next()) {
+                    this.setAnswerId((int) rs.getLong(1));
+                }
+
+                conn.close();
+
+                return this;
+            } else {
+                System.out.println("No connection!!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 }
